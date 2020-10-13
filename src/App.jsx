1b6c0e43a7f7740bb90cb6629ea0dae5ae3b6df7
Deepa <deepabollobhai@gmail.com>
2020-10-13 16:27:00 +0530

    space-x header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import Header from './components/header/Header.jsx';
+import './styles.css'
 function App() {
   return (
     <div className="App">
-      
-      space-x
+     <Header/>
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+class Header extends React.Component{
+render(){
+    return(<div className='header'><h1>space x</h1></div>)
+}
+}
+export default Header; 
