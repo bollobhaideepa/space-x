7bbc272fe1e22af8192b299e2c55fb216c4b0bf7
Deepa <deepabollobhai@gmail.com>
2020-10-14 11:31:36 +0530

    applying props to the component to reuse the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from './components/header/Header.jsx';
 import './styles.css'
+import Launch from './components/Launch/Launch.jsx'
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Launch 
+     title="tittle" 
+     date="12/34/21" 
+     description="description" 
+     drawing="https://farm2.staticflickr.com/1648/23827554109_837b21739e_o.jpg"/>
     </div>
   );
 }
---
+++ src/components/Launch/Launch.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.css';
+class Launch extends React.Component{
+render(){
+    return(
+    <div className='launch'>
+        <img src={this.props.drawing} alt='launch'/>
+    <div className='content'>
+      <div className='data'>
+    <h2>{this.props.title}</h2>
+    <h2>{this.props.date}</h2>
+      </div>
+    <p>{this.props.description}</p>
+    </div>
+    </div>
+    )
+}
+}
+export default Launch;
